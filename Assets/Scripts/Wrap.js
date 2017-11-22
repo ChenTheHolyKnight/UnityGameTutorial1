a67eae64d1402770c31707b1b7ae99424e73d9a6
Chen <czha959@aucklanduni.ac.nz>
2017-11-22 23:26:29 +1300

    Following Unity tutorial 2017

---
+++ Assets/Scripts/Wrap.js
@@ -0,0 +1,12 @@
+﻿var wrapTarget : Transform;
+
+function Update() {
+    
+}
+
+function OnTriggerEnter(col : Collider) {
+    if (col.gameObject.tag == "wrap001") {
+        this.transform.position = wrapTarget.position;
+
+    }
+}
