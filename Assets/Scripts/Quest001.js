f9f0640090e3dc0d2b31be1c321a84c4ca7451ad
Chen <czha959@aucklanduni.ac.nz>
2017-11-28 17:40:48 +1300

    Finish tutorial 14

---
+++ Assets/Scripts/Quest001.js
@@ -0,0 +1,45 @@
+﻿import UnityEngine.UI;
+
+var TextBoxOnCheck: int = 0;
+var MessageBox: GameObject;
+var TextBox: GameObject;
+var TextMessage: String;
+var QuestBox: GameObject;
+var QuestText: GameObject;
+var QuestName: String;
+
+function OnMouseDown() {
+    if (TextBoxOnCheck == 0) {
+        TextBoxOnCheck = 1;
+        MessageBox.SetActive(true);
+        TextBox.GetComponent.<Text>().text = TextMessage;
+        QuestName = "Active Quest: 'Recover the loot'";
+        QuestText.GetComponent.<Text>().text = QuestName;
+    } else {
+        TextBoxOnCheck = 0;
+        MessageBox.SetActive(false);
+        TextMessage = "Get Going then!";
+
+    }
+
+
+}
+
+function Update() {
+    if (Input.GetButtonDown("Submit")) {
+        if (TextBoxOnCheck == 1) {
+            MessageBox.SetActive(false);
+            TextBoxOnCheck = 0;
+            TextMessage = "Get Going then!";
+        }
+    }
+
+    if (Input.GetButtonDown("Cancel")) {
+        if (TextBoxOnCheck == 1) {
+            MessageBox.SetActive(false);
+            TextBoxOnCheck = 0;
+            TextMessage = "Get Going then!";
+        }
+    }
+
+}
